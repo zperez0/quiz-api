ed9ad860bfc83ebd2e4a4f99736d62c9cc052418
Yazmin Perez <zoeperez333@gmail.com>
2022-09-03 14:07:55 -0700

    adjust spacing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Settings from "./components/Settings";
 import Questions from "./components/Questions";
 import FinalScore from "./components/FinalScore";
+import { Container, Box } from "@mui/system";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={ <Settings />} />
-        <Route path="/questions" element={ <Questions />} />
-        <Route path="/score" element={<FinalScore />} />
-      </Routes>
+      <Container maxWidth="sm">
+        <Box textAlign="center" mt={5}>
+          <Routes>
+            <Route path="/" element={<Settings />} />
+            <Route path="/questions" element={<Questions />} />
+            <Route path="/score" element={<FinalScore />} />
+          </Routes>
+        </Box>
+      </Container>
     </Router>
   );
 }
