79e2ba587f97d7cbb3c5b44ef6a49f3695aefbb4
Yazmin Perez <zoeperez333@gmail.com>
2022-09-03 12:50:46 -0700

    add dispatch, navigate, useSelector & handleReturnHome

--- src/components/FinalScore.js
+++ src/components/FinalScore.js
@@ -1,11 +1,30 @@
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { handleAmountChange, handleScoreChange } from "../redux/actions";
 
-function FinalScore() {
+const FinalScore = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { score } = useSelector((state) => state);
+
+  const handleReturnHome = () => {
+    dispatch(handleScoreChange(0));
+    dispatch(handleAmountChange(10));
+    navigate("/");
+  };
   return (
-    <div>
-      <h1>FinalScore</h1>
-    </div>
+    <Box mt={3}>
+      <Typography variant="h3" fontWeight="bold" mb={3}>
+        Final Score {score}
+      </Typography>
+      <Button onClick={handleReturnHome} variant="outlined">
+        return home
+      </Button>
+    </Box>
   );
-}
+};
 
 export default FinalScore;
