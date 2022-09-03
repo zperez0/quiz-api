abf2e93635ed4079e16c26b978d68e4fa6710eb6
Yazmin Perez <zoeperez333@gmail.com>
2022-09-03 10:54:02 -0700

    update reducer(). add switch() with action types

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,13 +1,49 @@
+import {
+  CHANGE_CATEGORY,
+  CHANGE_DIFFICULTY,
+  CHANGE_TYPE,
+  CHANGE_AMOUNT,
+  CHANGE_SCORE,
+} from "./actionTypes";
+
 const initialState = {
   question_category: "",
   question_difficulty: "",
   question_type: "",
-  amount_of_question: 10,
+  question_amount: 10,
   score: 0,
 };
 
 const reducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case CHANGE_CATEGORY:
+      return {
+        ...state,
+        question_category: action.payload,
+      };
+    case CHANGE_DIFFICULTY:
+      return {
+        ...state,
+        question_difficulty: action.payload,
+      };
+    case CHANGE_TYPE:
+      return {
+        ...state,
+        question_type: action.payload,
+      };
+    case CHANGE_AMOUNT:
+      return {
+        ...state,
+        question_amount: action.payload,
+      };
+    case CHANGE_SCORE:
+      return {
+        ...state,
+        score: action.payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
