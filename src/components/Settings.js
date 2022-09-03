0a2bf762a39f25d0e7b944c7ba33e913284459ea
Yazmin Perez <zoeperez333@gmail.com>
2022-09-03 09:42:37 -0700

    import useAxios, add loading & error conditionals. add difficultyOptions & typeOptions

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,10 +1,40 @@
 import React from "react";
-import { Button, Typography } from "@mui/material";
+import { Button, CircularProgress, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import SelectField from "./SelectField";
 import TextFieldComp from "./TextFieldComp";
+import useAxios from "../hooks/useAxios";
 
 const Settings = () => {
+  const { response, error, loading } = useAxios({ url: "/api_category.php" });
+
+  if (loading) {
+    return (
+      <Box mt={20}>
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  if (error) {
+    return (
+      <Typography variant="h6" mt={20} color="red">
+        Something went wrong!
+      </Typography>
+    );
+  }
+
+  const difficultyOptions = [
+    { id: "easy", name: "Easy" },
+    { id: "medium", name: "Medium" },
+    { id: "hard", name: "Hard" },
+  ];
+
+  const typeOptions = [
+    { id: "multiple", name: "Multiple Choice" },
+    { id: "boolean", name: "True/False" },
+  ];
+
   const handleSubmit = (event) => {
     event.preventDefault();
   };
@@ -15,9 +45,9 @@ const Settings = () => {
         Quiz
       </Typography>
       <form onSubmit={handleSubmit}>
-        <SelectField label="Category" />
-        <SelectField label="Difficulty" />
-        <SelectField label="Type" />
+        <SelectField options={response.trivia_categories} label="Category" />
+        <SelectField options={difficultyOptions} label="Difficulty" />
+        <SelectField options={typeOptions} label="Type" />
         <TextFieldComp />
         <Box mt={3} width="100%">
           <Button fullWidth variant="contained" type="submit">
