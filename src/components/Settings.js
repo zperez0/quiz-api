e03eeacdf980c10b539d71b148ed1cc90a0a4d02
Yazmin Perez <zoeperez333@gmail.com>
2022-09-02 21:49:44 -0700

    add TextFieldComp

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import SelectField from "./SelectField";
+import TextFieldComp from "./TextFieldComp";
 
-function Settings() {
+const Settings = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Settings</h1>
-    </div>
+    <>
+      <Typography variant="h2" fontWeight="bold">
+        Quiz
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <SelectField label="Category" />
+        <SelectField label="Difficulty" />
+        <SelectField label="Type" />
+        <TextFieldComp />
+        <Box mt={3} width="100%">
+          <Button fullWidth variant="contained" type="submit">
+            Start
+          </Button>
+        </Box>
+      </form>
+    </>
   );
-}
+};
 
 export default Settings;
