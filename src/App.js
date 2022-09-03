6ddbfc559d1131b8faaf26bd41c4861fc4eaae7c
Yazmin Perez <zoeperez333@gmail.com>
2022-09-02 21:01:35 -0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Settings from "./components/Settings";
+import Questions from "./components/Questions";
+import FinalScore from "./components/FinalScore";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={ <Settings />} />
+        <Route path="/questions" element={ <Questions />} />
+        <Route path="/score" element={<FinalScore />} />
+      </Routes>
+    </Router>
   );
 }
 
