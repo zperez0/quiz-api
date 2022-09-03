53fa552357d27fd622b1b6a1cd1061d7015d14e1
Yazmin Perez <zoeperez333@gmail.com>
2022-09-03 10:02:27 -0700

    add useAxios, setup basic outline for Questions page.

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,11 +1,28 @@
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import useAxios from "../hooks/useAxios";
 
-function Questions() {
+const Questions = () => {
+
+  let apiUrl = `/api.php?amount=10`
+
+  const { response, loading } = useAxios({ url: apiUrl })
+  console.log(response)
+
   return (
-    <div>
-      <h1>Questions</h1>
-    </div>
+    <Box>
+      <Typography variant="h4">Questions 1</Typography>
+      <Typography mt={5}>question goes here</Typography>
+      <Box mt={2}>
+        <Button variant="contained">Answer 1</Button>
+      </Box>
+      <Box mt={2}>
+        <Button variant="contained">Answer 2</Button>
+      </Box>
+      <Box mt={5}>Score: 2 / 6</Box>
+    </Box>
   );
-}
+};
 
 export default Questions;
