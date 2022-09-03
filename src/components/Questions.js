e5fcc48dd7dc152d812e91412abfcc0681ced636
Yazmin Perez <zoeperez333@gmail.com>
2022-09-03 12:42:12 -0700

    add navigate, dispatch, useEffect & decode. add conditionals for urls. update handleClickAnswer

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,33 +1,88 @@
-import { Button, Typography } from "@mui/material";
+import { Button, CircularProgress, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import React from "react";
-import { useSelector } from "react-redux";
+import { decode } from "html-entities";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import useAxios from "../hooks/useAxios";
+import { handleScoreChange } from "../redux/actions";
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
 
 const Questions = () => {
-  const { 
+  const {
     question_category,
     question_difficulty,
     question_type,
     question_amount,
-  } = useSelector(state => state);
-  console.log( question_category, question_difficulty, question_type )
+    score,
+  } = useSelector((state) => state);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  let apiUrl = `/api.php?amount=10`
+  let apiUrl = `/api.php?amount=${question_amount}`;
 
-  const { response, loading } = useAxios({ url: apiUrl })
+  if (question_category) {
+    apiUrl = apiUrl.concat(`&category=${question_category}`);
+  }
+  if (question_difficulty) {
+    apiUrl = apiUrl.concat(`&difficulty=${question_difficulty}`);
+  }
+  if (question_type) {
+    apiUrl = apiUrl.concat(`&type=${question_type}`);
+  }
+
+  const { response, loading } = useAxios({ url: apiUrl });
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    if (response?.results.length) {
+      const question = response.results[questionIndex];
+      let answers = [...question.incorrect_answers];
+      answers.splice(
+        getRandomInt(question.incorrect_answers.length),
+        0,
+        question.correct_answer
+      );
+      setOptions(answers);
+    }
+  }, [response, questionIndex]);
+
+  if (loading) {
+    return (
+      <Box mt={20}>
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  const handleClickAnswer = (event) => {
+    const question = response.results[questionIndex];
+    if (event.target.textContent === question.correct_answer) {
+      dispatch(handleScoreChange(score + 1));
+    }
+    if (questionIndex + 1 < response.results.length) {
+      setQuestionIndex(questionIndex + 1);
+    } else {
+      navigate("/score");
+    }
+  };
 
   return (
     <Box>
-      <Typography variant="h4">Questions 1</Typography>
-      <Typography mt={5}>question goes here</Typography>
-      <Box mt={2}>
-        <Button variant="contained">Answer 1</Button>
-      </Box>
-      <Box mt={2}>
-        <Button variant="contained">Answer 2</Button>
-      </Box>
-      <Box mt={5}>Score: 2 / 6</Box>
+      <Typography variant="h4">Questions {questionIndex + 1}</Typography>
+      <Typography mt={5}>{decode(response.results[questionIndex].question)}</Typography>
+      {options.map((data, id) => (
+        <Box mt={2} key={id}>
+          <Button onClick={handleClickAnswer} variant="contained">
+            {decode(data)}
+          </Button>
+        </Box>
+      ))}
+      <Box mt={5}>Score: {score}</Box>
     </Box>
   );
 };
