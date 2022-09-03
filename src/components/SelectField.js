5790f919acf50fae9b5eee422c33e061b971a91a
Yazmin Perez <zoeperez333@gmail.com>
2022-09-03 11:30:39 -0700

    add switch() to handle changes &  useDispatch()

--- src/components/SelectField.js
+++ src/components/SelectField.js
@@ -1,14 +1,34 @@
 import React, { useState } from "react";
 import { Box } from "@mui/system";
 import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { useDispatch } from "react-redux";
+import {
+  handleCategoryChange,
+  handleDifficultyChange,
+  handleTypeChange,
+} from "../redux/actions";
 
 const SelectField = (props) => {
   const { label, options } = props;
   const [value, setValue] = useState("");
+  const dispatch = useDispatch();
 
   const handleChange = (event) => {
-    setValue(event.target.value)
-  }
+    setValue(event.target.value);
+    switch (label) {
+      case "Category":
+        dispatch(handleCategoryChange(event.target.value));
+        break;
+      case "Difficulty":
+        dispatch(handleDifficultyChange(event.target.value));
+        break;
+      case "Type":
+        dispatch(handleTypeChange(event.target.value));
+        break;
+      default:
+        return;
+    }
+  };
 
   return (
     <Box mt={3} width="100%">
